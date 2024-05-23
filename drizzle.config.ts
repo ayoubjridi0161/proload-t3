1cc621dd6f3c62d5270bf4a621e5af2a0e44203b
ayoubjridi0161 <118389959+ayoubjridi0161@users.noreply.github.com>
2024-05-23 16:43:00 +0100

    -fixed some issues regarding deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { type Config } from "drizzle-kit";
-
 import { env } from "~/env";
-
-export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["proload-t3_*"],
-} satisfies Config;
+import { defineConfig } from "drizzle-kit"
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/server/db/schema.ts",
+    //driver: "turso" // optional and used only if `aws-data-api`, `turso`, `d1-http`(WIP) or `expo` are used
+    dbCredentials: {
+      url: env.POSTGRES_URL,    }
+})
--- src/app/workouts/page.tsx
+++ src/app/workouts/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import CreateWorkout from '~/components/ui/workouts/CreateWorkout'
 
-type Props = {}
-
-export default function page({}: Props) {
+export default function page() {
   return (
     <div className='h-full'>
       <CreateWorkout />
