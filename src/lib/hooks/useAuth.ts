d90526e4e191e571b34244b21d587a1f45527752
ayoubjridi0161 <118389959+ayoubjridi0161@users.noreply.github.com>
2025-02-11 18:11:49 +0100

    fixed useAuth

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -7,16 +7,16 @@ type authResponse = {
     user : User | null,
     error : string | null
 }
-// export const useAuth = async ()=>{
-//     const session = await auth()
-//     let error = null
-//     let user = null
-//     let sessionToken = null
-//     if(!session) {
-//         error = "no session found"
-//     }else{
-//         sessionToken = session.sessionToken
-//         user = session.user
-//     }
-//     return {sessionToken,user,error} as authResponse
-// }
+export const useAuth = async ()=>{
+    const session = await auth()
+    let error = null
+    let user = null
+    let sessionToken = null
+    if(!session) {
+        error = "no session found"
+    }else{
+        sessionToken = session.sessionToken || "noToken"
+        user = session.user
+    }
+    return {sessionToken,user,error} as authResponse
+}
