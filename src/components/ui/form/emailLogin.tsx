3bfe7d64b1b4b12225caf119063593dc5678e16f
ayoubjridi0161 <jridiayoub@isitc.u-sousse.tn>
2025-06-15 15:27:30 +0100

    style(emailLogin): adjust button width and alignment for improved layout

--- src/components/ui/form/emailLogin.tsx
+++ src/components/ui/form/emailLogin.tsx
@@ -20,7 +20,7 @@ const EmailLogin = () => {
                         <Button
                             type="submit"
                             variant="outline"
-                            className="w-full flex items-center justify-center gap-2"
+                            className="w-fit mx-auto flex items-center justify-center gap-2"
                         >
                             <svg className="w-6 h-6" viewBox="0 0 24 24">
                                 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"></path>
@@ -35,7 +35,7 @@ const EmailLogin = () => {
                     <Separator className="my-4" />
                     <form className="w-full" action={signInWithResend}>
                         <Input
-                            className="bg-white text-base mb-3 w-full border text-xtraText border-gray-300 rounded-md px-3 py-2"
+                            className="bg-white text-base mb-3 w-full border text-xtraText dark:text-xtraDarkText border-gray-300 rounded-md px-3 py-2"
                             type="email"
                             placeholder="Email"
                             name="email"
@@ -43,7 +43,7 @@ const EmailLogin = () => {
                         <Button
                             type="submit"
                             variant="outline"
-                            className="w-full flex items-center justify-center gap-2 "
+                            className="w-fit mx-auto flex items-center justify-center gap-2 "
                         >
                             Continue
                             <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
